9003213c5244bb952d57b02c92aa2115a3cf17b5
gennardo.gennardo <gennardo.gennardo@bytedance.com>
2023-12-19 20:48:23 +0800

    feat: login page design

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,19 @@
+import { Button, Form } from '@douyinfe/semi-ui';
+import styles from './index.module.scss';
+
 export default function LoginPage() {
+  function handleLogin(values: unknown) {
+    console.log('hahaha ', values);
+  }
   return (
-    <div>login page</div>
-  )
+    <div className={styles.loginPage}>
+      <Form className={styles.loginForm} onSubmit={handleLogin}>
+        <Form.Input field="username" label="Username" placeholder="Enter your username" />
+        <Form.Input field="password" label="Password" placeholder="Enter your password" mode="password" />
+        <Button className={styles.submitBtn} htmlType="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
