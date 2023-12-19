928acbb849ce958c49e4581a5664aae865546f3e
DelphicGen <50311626+DelphicGen@users.noreply.github.com>
2023-12-19 20:20:14 +0800

    feat: setup routing and prettier (#1)
    * feat: setup routing and prettier
    
    * feat: typescript and sass setup:
    
    * chore: remove unnecessary config
    
    ---------
    
    Co-authored-by: gennardo.gennardo <gennardo.gennardo@bytedance.com>

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120,
+  endOfLine: 'auto',
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: true,
+  tabWidth: 2,
+  arrowParens: 'avoid'
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginPage from './pages/login';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <LoginPage />,
+    },
+  ]);
   return (
-    <div>Hello World</div>
+    <div>
+      Hello World
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
