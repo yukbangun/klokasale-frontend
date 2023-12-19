cd4a253a1cb43db6f114af07c678baa231fdc886
gennardo.gennardo <gennardo.gennardo@bytedance.com>
2023-12-19 22:56:37 +0800

    feat: add useEffet to check if user is logged in

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from 'react';
 import NavigationBar from 'src/components/navigation-bar';
 
 export default function DashboardPage() {
+  useEffect(() => {
+    // TODO: check if user is logged in
+  }, []);
   return (
     <div>
       <NavigationBar />
