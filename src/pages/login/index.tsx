d25aba1f7d3abe0e33f2e8a33929f81830eca733
gennardo.gennardo <gennardo.gennardo@bytedance.com>
2023-12-19 21:10:48 +0800

    feat: polish login page styling

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,21 @@
-import { Button, Form } from '@douyinfe/semi-ui';
+import { Button, Form, Spin } from '@douyinfe/semi-ui';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './index.module.scss';
 
 export default function LoginPage() {
+  const navigate = useNavigate();
+  const [isSubmitting, setIsSubmitting] = useState(false);
   function handleLogin(values: unknown) {
-    console.log('hahaha ', values);
+    try {
+      // TODO: handle login
+      setIsSubmitting(true);
+      navigate('/');
+    } catch (e) {
+      // TODO: show error toast
+    } finally {
+      setIsSubmitting(false);
+    }
   }
   return (
     <div className={styles.loginPage}>
@@ -11,7 +23,7 @@ export default function LoginPage() {
         <Form.Input field="username" label="Username" placeholder="Enter your username" />
         <Form.Input field="password" label="Password" placeholder="Enter your password" mode="password" />
         <Button className={styles.submitBtn} htmlType="submit">
-          Submit
+          {isSubmitting ? <Spin /> : 'Submit'}
         </Button>
       </Form>
     </div>
