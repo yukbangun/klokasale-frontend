805cdc1e5579592e1b5665d58b6bc7ca1f2a9e10
gennardo.gennardo <gennardo.gennardo@bytedance.com>
2023-12-21 20:15:39 +0800

    feat: go to login if not logged in and store tree select on nav

--- src/components/navigation-bar/index.tsx
+++ src/components/navigation-bar/index.tsx
@@ -1,7 +1,45 @@
-import { Nav } from '@douyinfe/semi-ui';
+import { Nav, TreeSelect } from '@douyinfe/semi-ui';
 import styles from './index.module.scss';
 
 export default function NavigationBar() {
+  const treeData = [
+    {
+      label: 'Store Group 1',
+      value: 'store_group_1',
+      key: 'store_group_1',
+      disabled: true,
+      children: [
+        {
+          label: 'Store A',
+          value: 'store_a',
+          key: 'store_a',
+        },
+        {
+          label: 'Store B',
+          value: 'store_b',
+          key: 'store_b',
+        },
+      ],
+    },
+    {
+      label: 'Store Group 2',
+      value: 'store_group_2',
+      key: 'store_group_2',
+      disabled: true,
+      children: [
+        {
+          label: 'Store C',
+          value: 'store_c',
+          key: 'store_c',
+        },
+        {
+          label: 'Store D',
+          value: 'store_d',
+          key: 'store_d',
+        },
+      ],
+    },
+  ];
   return (
     <Nav
       className={styles.nav}
@@ -23,7 +61,14 @@ export default function NavigationBar() {
       ]}
       onSelect={key => console.log(key)}
       header={{
-        text: 'KlokaSale',
+        children: (
+          <TreeSelect
+            style={{ width: 300 }}
+            dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+            treeData={treeData}
+            placeholder="Please select"
+          />
+        ),
       }}
     />
   );
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,16 @@
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import NavigationBar from 'src/components/navigation-bar';
+import { LocalStorageKey } from 'src/constants/local-storage';
+import { isNullish } from 'src/utils/nullish';
 
 export default function DashboardPage() {
+  const navigate = useNavigate();
   useEffect(() => {
-    // TODO: check if user is logged in
+    const bearerToken = localStorage.getItem(LocalStorageKey.BearerToken);
+    if (isNullish(bearerToken)) {
+      navigate('/login');
+    }
   }, []);
   return (
     <div>
