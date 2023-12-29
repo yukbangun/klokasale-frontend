72d478a325332ed3dd3c4213c363f9c11cdb63b9
DelphicGen <50311626+DelphicGen@users.noreply.github.com>
2023-12-29 21:26:59 +0800

    Feat/nav (#3)
    * feat: login page design
    
    * feat: polish login page styling
    
    * feat: remove unused codes
    
    * feat: polish login page
    
    * feat: nav
    
    * feat: add required validator
    
    * feat: add useEffet to check if user is logged in
    
    * feat: check if user is logged in
    
    * feat: add more todo
    
    * feat: go to login if not logged in and store tree select on nav
    
    * feat: set query params when changing nav
    
    * feat: set query params when changing nav
    
    * fix: nav to use route instead of query params
    
    * fix: clicking back and forth in browser updates nav
    
    * feat: update nav to no longer support tabs
    
    * chore: remove unused codes
    
    * feat: add default value as pos
    
    ---------
    
    Co-authored-by: gennardo.gennardo <gennardo.gennardo@bytedance.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,60 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LoginPage from './pages/login';
 import DashboardPage from './pages/dashboard';
+import MasterBarangPage from './pages/master/barang';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <DashboardPage />,
+      children: [
+        {
+          index: true,
+          element: <div>pos</div>,
+        },
+        {
+          path: 'pos',
+          element: <div>pos</div>,
+        },
+        {
+          path: 'master',
+          children: [
+            {
+              path: 'trademark',
+              element: <div>trademark</div>,
+            },
+            {
+              path: 'barang',
+              element: <MasterBarangPage />,
+            },
+            {
+              path: 'unit',
+              element: <div>unit</div>,
+            },
+            {
+              path: 'supplier',
+              element: <div>supplier</div>,
+            },
+            {
+              path: 'pelanggan',
+              element: <div>pelanggan</div>,
+            },
+            {
+              path: 'kas_dan_biaya',
+              element: <div>kas_dan_biaya</div>,
+            },
+            {
+              path: 'piutang',
+              element: <div>piutang</div>,
+            },
+            {
+              path: 'hutang',
+              element: <div>hutang</div>,
+            },
+          ],
+        },
+      ],
     },
     {
       path: '/login',
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,22 @@
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import NavigationBar from 'src/components/navigation-bar';
+import { LocalStorageKey } from 'src/constants/local-storage';
+import { isNullish } from 'src/utils/nullish';
+import styles from './index.module.scss';
+
 export default function DashboardPage() {
-  return <div>dashboard page</div>;
+  const navigate = useNavigate();
+  useEffect(() => {
+    const bearerToken = localStorage.getItem(LocalStorageKey.BearerToken);
+    if (isNullish(bearerToken)) {
+      navigate('/login');
+    }
+  }, []);
+  return (
+    <div className={styles.dashboardPage}>
+      <NavigationBar />
+      <Outlet />
+    </div>
+  );
 }
