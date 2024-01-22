f891269c4e87cbd3b9492dec63050e97546841f7
DelphicGen <50311626+DelphicGen@users.noreply.github.com>
2024-01-22 23:01:59 +0800

    Feat/login be (#10)
    * feat: login page design
    
    * feat: polish login page styling
    
    * feat: remove unused codes
    
    * feat: polish login page
    
    * feat: nav
    
    * feat: add required validator
    
    * feat: add useEffet to check if user is logged in
    
    * feat: check if user is logged in
    
    * feat: add more todo
    
    * feat: go to login if not logged in and store tree select on nav
    
    * feat: set query params when changing nav
    
    * feat: set query params when changing nav
    
    * fix: nav to use route instead of query params
    
    * fix: clicking back and forth in browser updates nav
    
    * feat: update nav to no longer support tabs
    
    * chore: remove unused codes
    
    * feat: add default value as pos
    
    * feat: generate services and make a call
    
    * feat: integrate login with BE
    
    * feat: update json
    
    * feat: login
    
    ---------
    
    Co-authored-by: gennardo.gennardo <gennardo.gennardo@bytedance.com>

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,15 +1,15 @@
 import { useEffect } from 'react';
+import { useCookies } from 'react-cookie';
 import { Outlet, useNavigate } from 'react-router-dom';
 import NavigationBar from 'src/components/navigation-bar';
-import { LocalStorageKey } from 'src/constants/local-storage';
 import { isNullish } from 'src/utils/nullish';
 import styles from './index.module.scss';
 
 export default function DashboardPage() {
   const navigate = useNavigate();
+  const [cookies] = useCookies();
   useEffect(() => {
-    const bearerToken = localStorage.getItem(LocalStorageKey.BearerToken);
-    if (isNullish(bearerToken)) {
+    if (isNullish(cookies?.token)) {
       navigate('/login');
     }
   }, []);
